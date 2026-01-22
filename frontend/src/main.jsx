a6c97af3caf52bd220ae9c0c5a33efb0f24f5379
Fatima ezzahra Lamlih <fatimaezzahralamlih776@gmail.com>
2026-01-22 20:16:36 +0100

    main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,2 +1,12 @@
 // 1337Jury - Main Entry Point
 // This file is for: FATYZA (Frontend Developer)
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
